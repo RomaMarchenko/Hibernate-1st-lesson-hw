a4705c309244658fa05c897b6e7550b9250cd05b
RomaMarchenko <52100927+RomaMarchenko@users.noreply.github.com>
2020-08-11 13:39:06 +0300

    Update ProductRepository.java

--- src/main/java/lesson5/ProductRepository.java
+++ src/main/java/lesson5/ProductRepository.java
@@ -4,45 +4,42 @@ import org.hibernate.Session;
 
 public class ProductRepository {
     public static void save(Product product) {
-        Session session = new HibernateUtils().createSessionFactory().openSession();
-        session.getTransaction().begin();
+        try(Session session = new HibernateUtils().createSessionFactory().openSession()) {
+            session.getTransaction().begin();
 
-        session.save(product);
+            session.save(product);
 
-        session.getTransaction().commit();
+            session.getTransaction().commit();
 
-        System.out.println("Product with ID: " + product.getId() + " was saved to DB.");
-
-        session.close();
+            System.out.println("Product with ID: " + product.getId() + " was saved to DB.");
+        }
     }
 
     public static void delete(long id) {
-        Session session = new HibernateUtils().createSessionFactory().openSession();
-        session.getTransaction().begin();
+        try(Session session = new HibernateUtils().createSessionFactory().openSession()) {
+            session.getTransaction().begin();
 
-        Product product = session.get(Product.class, id);
+            Product product = session.get(Product.class, id);
 
-        if(product != null) {
-            session.delete(product);
+            if (product != null) {
+                session.delete(product);
+            }
+
+            session.getTransaction().commit();
+
+            System.out.println("Product with ID: " + id + " was deleted from DB.");
         }
-
-        session.getTransaction().commit();
-
-        System.out.println("Product with ID: " + id + " was deleted from DB.");
-
-        session.close();
     }
 
     public static void update(Product product) {
-        Session session = new HibernateUtils().createSessionFactory().openSession();
-        session.getTransaction().begin();
+        try(Session session = new HibernateUtils().createSessionFactory().openSession()) {
+            session.getTransaction().begin();
 
-        session.update(product);
+            session.update(product);
 
-        session.getTransaction().commit();
+            session.getTransaction().commit();
 
-        System.out.println("Product with ID: " + product.getId() + " was updated.");
-
-        session.close();
+            System.out.println("Product with ID: " + product.getId() + " was updated.");
+        }
     }
 }
